36d94ac7e97e133616e696e97a97ff58ca3627e6
m-miron <mironmaricarmen@gmail.com>
2023-05-12 15:10:45 -0400

    Added vehicle inventory array list instantiation, Dealership constructor and null dealership methods. Also a getAllVehicles, addVehicle, and removeVehicle method.

--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,12 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
 
     private String name;
     private String address;
     private String phone;
+    private ArrayList<Vehicle> inventory;
 
-    public Dealership(String name, String address, String phone) {
+    public Dealership(String name, String address, String phone, ArrayList<Vehicle> inventory) {
         this.name = name;
         this.address = address;
         this.phone = phone;
+        this.inventory = inventory;
+    }
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType){
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles(){
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle anotherVehicle) {
+        inventory.add(anotherVehicle);
+    }
+
+    public void removeVehicle(Vehicle anotherVehicle){
+        inventory.remove(anotherVehicle);
     }
 }
