d9af96a8af87fb8d1f7ba4745cfd06436f6b8e19
m-miron <mironmaricarmen@gmail.com>
2023-05-12 14:35:23 -0400

    Initial commit

---
+++ src/main/java/Dealership.java
@@ -0,0 +1,12 @@
+public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+}
