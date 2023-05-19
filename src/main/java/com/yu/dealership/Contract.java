17477cb135ab55b80aa1a9e54ba213b42ccb97a5
m-miron <mironmaricarmen@gmail.com>
2023-05-19 13:37:57 -0400

    Modified refactoring naming mistakes.

---
+++ src/main/java/com/yu/dealership/Contract.java
@@ -0,0 +1,4 @@
+package com.yu.dealership;
+
+public class Contract {
+}
--- src/main/java/com/yu/dealership/DealershipFileManager.java
+++ src/main/java/com/yu/dealership/DealershipFileManager.java
@@ -4,13 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 public class DealershipFileManager {
 
     public static String dealershipFileName = "01-DealershipVehicleList";
+
     //        private final ArrayList<com.yu.dealership.Vehicle> vehiclesList = new ArrayList<>();
     public Dealership getDealership() {
+        Dealership anotherDealership = null;
         try {
             // To open the dealership file for reading
             BufferedReader dealershipFileReader = new BufferedReader(new FileReader(dealershipFileName));
@@ -23,7 +24,7 @@ public class DealershipFileManager {
             String phone = dealershipDetails[2];
 
             // Create a new com.yu.dealership.Dealership object with the extracted information
-            Dealership anotherDealership = new Dealership(name, address, phone);
+            anotherDealership = new Dealership(name, address, phone);
 
 
             // Read the remaining lines of the file containing vehicle information
@@ -33,48 +34,51 @@ public class DealershipFileManager {
                     String[] vehicleDetails = fileLine.split("\\|");
                     // Extract vehicle information
                     int vin = Integer.parseInt(vehicleDetails[0]);
-                    int year = Integer.parseInt(vehicleDetails[1]);
-                    String make = vehicleDetails[2];
-                    String model = vehicleDetails[3];
-                    String vehicleType = vehicleDetails[4];
-                    String color = vehicleDetails[5];
+                    String color = vehicleDetails[1];
+                    String vehicleType = vehicleDetails[2];
+                    int year = Integer.parseInt(vehicleDetails[3]);
+                    String make = vehicleDetails[4];
+                    String model = vehicleDetails[5];
                     int odometer = Integer.parseInt(vehicleDetails[6]);
                     double price = Double.parseDouble(vehicleDetails[7]);
                     // Create a new com.yu.dealership.Vehicle object with the extracted information
-                    Vehicle vehicle = new Vehicle(vin, color, vehicleType, year, make, model,odometer, price);
+                    Vehicle vehicle = new Vehicle(vin, color, vehicleType, year, make, model, odometer, price);
                     // Add the com.yu.dealership.Vehicle object to the com.yu.dealership.Dealership's inventory
                     anotherDealership.addVehicle(vehicle);
                 }
             }
-
             dealershipFileReader.close();
-
-            return anotherDealership;// Return the newly constructed com.yu.dealership.Dealership object
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return anotherDealership;// Return the newly constructed com.yu.dealership.Dealership object
     }
 
 
     public void saveDealership(Dealership anotherDealership) {
         //TODO: Implement logic for saving a new dealership.
-        try (FileWriter fileWriter = new FileWriter(dealershipFileName, true)) {
-            List<Vehicle> vehicles = anotherDealership.getAllVehicles();
-            for (Vehicle vehicle : vehicles) {
-            fileWriter.write(String.format("%d|%s|%s|%d|%s|%s|%d|.2f\n",
-            vehicle.getVin(),
-            vehicle.getColor(),
-            vehicle.getVehicleType(),
-            vehicle.getYear(),
-            vehicle.getMake(),
-            vehicle.getModel(),
-            vehicle.getOdometer(),
-            vehicle.getPrice()));
+        try (FileWriter fileWriter = new FileWriter(dealershipFileName, false)) {
+//            List<Vehicle> vehicles = anotherDealership.getAllVehicles();
+            //Write first line: Dealership Info
+            fileWriter.write(String.format("%s|%s|%s",
+                    anotherDealership.getName(),
+                    anotherDealership.getAddress(),
+                    anotherDealership.getPhone()));
 
-            fileWriter.write(vehicle.vehicleListFormat());
-            fileWriter.flush();
-            fileWriter.close();
+            for (Vehicle vehicle : anotherDealership.getAllVehicles()) {
+                String nVehicle =
+                        String.format("\n%d|%s|%s|%d|%s|%s|%d|%.2f",
+                                vehicle.getVin(),
+                                vehicle.getColor(),
+                                vehicle.getVehicleType(),
+                                vehicle.getYear(),
+                                vehicle.getMake(),
+                                vehicle.getModel(),
+                                vehicle.getOdometer(),
+                                vehicle.getPrice());
+
+                fileWriter.write(nVehicle);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
